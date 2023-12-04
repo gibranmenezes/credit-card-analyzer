97d5d34c7a8cfb9ad0d3d30e5a32a7ed8fedd5f1
gibranmenezes <gibranquimica@gmail.com>
2023-12-04 13:57:53 -0300

    feat creating Controller

---
+++ ms-creditanalyzer/src/main/java/com/creditcardanalyzer/mscreditanalyzer/controller/CreditAnalyzerController.java
@@ -0,0 +1,15 @@
+package com.creditcardanalyzer.mscreditanalyzer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("credit-analisys")
+public class CreditAnalyzerController {
+
+    @GetMapping
+    public String status() {
+        return "ok";
+    }
+}
