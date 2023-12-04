148c964886660ced2e81eb9db9c603b105198367
gibranmenezes <gibranquimica@gmail.com>
2023-12-04 13:35:11 -0300

    feat creation of API MS-CreditAnalyzer

---
+++ ms-creditanalyzer/src/test/java/com/creditcardanalyzer/mscreditanalyzer/MsCreditanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditcardanalyzer.mscreditanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCreditanalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
